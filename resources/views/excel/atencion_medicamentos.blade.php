5a658fd8c91e5343e60b450ae4c782636ab04417
Your Name <you@example.com>
2023-06-19 19:47:13 -0500

    avancve

--- resources/views/excel/atencion_medicamentos.blade.php
+++ resources/views/excel/atencion_medicamentos.blade.php
@@ -25,10 +25,10 @@
         @foreach($medicamentos as $medicamento)
         <tr>
             <td></td>
-            <td style=" border: 2px solid black;text-align:center;">{{ $medicamento->created_at ? $medicamento->created_at:'' }}</td>
-            <td style=" border: 2px solid black;text-align:center;">{{ $medicamento->estacion && $medicamento->estacion->sede && $medicamento->estacion->sede->descripcion ?:'' }}</td>
-            <td style=" border: 2px solid black;text-align:center;">{{ $medicamento->estacion && $medicamento->estacion->nombre_estacion ?:'' }}</td>
-            <td style=" border: 2px solid black;text-align:center;">{{ strtoupper($medicamento->user && $medicamento->user->nombres) ?:'' }} {{ strtoupper($medicamento->user && $medicamento->user->apellidos) ?:'' }}</td>
+            <td style=" border: 2px solid black;text-align:center;">{{ $medicamento->created_at ? $medicamento->created_at?:'' }}</td>
+            <td style=" border: 2px solid black;text-align:center;">{{ $medicamento->estacion && $medicamento->estacion->sede && $medicamento->estacion->sede->descripcion ? $medicamento->estacion->sede->descripcion:'' }}</td>
+            <td style=" border: 2px solid black;text-align:center;">{{ $medicamento->estacion && $medicamento->estacion->nombre_estacion ? $medicamento->estacion->nombre_estacion:'' }}</td>
+            <td style=" border: 2px solid black;text-align:center;">{{ strtoupper($medicamento->user && $medicamento->user->nombres) ? $medicamento->user->nombres:'' }} {{ strtoupper($medicamento->user && $medicamento->user->apellidos) ? $medicamento->user->apellidos:'' }}</td>
             <td style=" border: 2px solid black;text-align:center;">{{ $medicamento->paciente->full_name ? $medicamento->paciente->full_name :'' }}</td>
             <td style=" border: 2px solid black;text-align:center;">{{ $medicamento->paciente->nro_registro ? $medicamento->paciente->nro_registro:''}}</td>
             <td style=" border: 2px solid black;text-align:center;">{{ $medicamento->paciente->numero_documento ? $medicamento->paciente->numero_documento:''}}</td>
