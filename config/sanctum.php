7fd2c0b48a8e43c075f0291d6df8a535881aa87f
Your Name <you@example.com>
2023-06-30 08:08:50 -0500

    agregado sactm

--- config/sanctum.php
+++ config/sanctum.php
@@ -13,12 +13,11 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', env('SANCTUM_STATEFUL_DOMAINS_2', sprintf(
         '%s%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
-        env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : '',
-        env('FRONTEND_URL_3') ? ','.parse_url(env('FRONTEND_URL_3'), PHP_URL_HOST) : ''
+        env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
 
     /*
